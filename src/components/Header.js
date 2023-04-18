7d13163569b3b0e22974ec4a0ee1c0076bcfabb8
Peter Jolley <peter@marango.com.au>
2023-04-18 15:40:48 +0930

    header component complete

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css';
+
+const Header = () => {
+    <header className="home-header">
+    <h2>My Political Blog</h2>
+    <h1>
+        <span>"</span> Please enjoy reading my Writings <span>"</span>
+    </h1>
+    </header>
+}
+
+export default Header;
